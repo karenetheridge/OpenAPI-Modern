7295c185e6316ad0bd37f96a041f4c06abe44298
Karen Etheridge <ether@cpan.org>
2021-10-30 11:42:50 -0700

    document extends base document

--- lib/JSON/Schema/Modern/Document/OpenAPI.pm
+++ lib/JSON/Schema/Modern/Document/OpenAPI.pm
@@ -2,8 +2,8 @@ use strict;
 use warnings;
 package JSON::Schema::Modern::Document::OpenAPI;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
-# ABSTRACT: ...
-# KEYWORDS: ...
+# ABSTRACT: One JSON Schema document
+# KEYWORDS: JSON Schema data validation request response OpenAPI
 
 our $VERSION = '0.001';
 
@@ -12,6 +12,9 @@ use if "$]" >= 5.022, experimental => 're_strict';
 no if "$]" >= 5.031009, feature => 'indirect';
 no if "$]" >= 5.033001, feature => 'multidimensional';
 no if "$]" >= 5.033006, feature => 'bareword_filehandles';
+use Moo;
+
+extends 'JSON::Schema::Modern::Document';
 
 1;
 __END__
