c825467325779e586c1a967f4da09a5b40096087
Karen Etheridge <ether@cpan.org>
2021-11-09 12:09:58 -0800

    basic object construction

--- lib/OpenAPI/Modern.pm
+++ lib/OpenAPI/Modern.pm
@@ -2,15 +2,68 @@ use strict;
 use warnings;
 package OpenAPI::Modern;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
-# ABSTRACT: ...
-# KEYWORDS: ...
+# ABSTRACT: Validate HTTP requests and responses against an OpenAPI document
+# KEYWORDS: validation evaluation json schema openapi http request response
 
 our $VERSION = '0.001';
 
-use 5.016;
+use 5.020;  # for fc, unicode_strings features
+use Moo;
+use strictures 2;
+use experimental qw(signatures postderef);
+use if "$]" >= 5.022, experimental => 're_strict';
 no if "$]" >= 5.031009, feature => 'indirect';
 no if "$]" >= 5.033001, feature => 'multidimensional';
 no if "$]" >= 5.033006, feature => 'bareword_filehandles';
+use Carp 'croak';
+use JSON::Schema::Modern 0.524;
+use JSON::Schema::Modern::Document::OpenAPI;
+use MooX::TypeTiny 0.002002;
+use Types::Standard 'InstanceOf';
+use namespace::clean;
+
+has openapi_document => (
+  is => 'ro',
+  isa => InstanceOf['JSON::Schema::Modern::Document::OpenAPI'],
+  required => 1,
+  handles => {
+    openapi_uri => 'canonical_uri', # Mojo::URL
+    openapi_schema => 'schema',     # hashref
+  },
+);
+
+# held separately because $document->evaluator is a weak ref
+has evaluator => (
+  is => 'ro',
+  isa => InstanceOf['JSON::Schema::Modern'],
+  required => 1,
+);
+
+around BUILDARGS => sub ($orig, $class, @args) {
+  my $args = $class->$orig(@args);
+
+  if (exists $args->{openapi_document}) {
+    $args->{evaluator} = $args->{openapi_document}->evaluator;
+  }
+  else {
+    # construct document out of openapi_uri, openapi_schema, evaluator, if provided.
+    croak 'missing required constructor arguments: either openapi_document, or openapi_uri'
+      if not exists $args->{openapi_uri};
+    croak 'missing required constructor arguments: either openapi_document, or openapi_schema'
+      if not exists $args->{openapi_schema};
+
+    $args->{evaluator} //= JSON::Schema::Modern->new(validate_formats => 1);
+    $args->{openapi_document} = JSON::Schema::Modern::Document::OpenAPI->new(
+      canonical_uri => $args->{openapi_uri},
+      schema => $args->{openapi_schema},
+      evaluator => $args->{evaluator},
+    );
+
+    $args->{evaluator}->add_schema($args->{openapi_document});
+  }
+
+  return $args;
+};
 
 1;
 __END__
@@ -21,25 +74,67 @@ __END__
 
   use OpenAPI::Modern;
 
-  ...
+  my $openapi = OpenAPI::Modern->new(
+    openapi_uri => 'openapi.yaml',
+    openapi_schema => {
+      openapi => '3.1.0',
+      info => {
+        title => 'Test API',
+        version => '1.2.3',
+      },
+      paths => {},
+    },
+  );
 
 =head1 DESCRIPTION
 
-...
+... TODO!
 
-=head1 FUNCTIONS/METHODS
+=head1 CONSTRUCTOR ARGUMENTS
 
-=head2 foo
+=head2 openapi_document
 
-...
+The L<JSON::Schema::Modern::Document::OpenAPI> document that holds the OpenAPI information to be
+used for validation. If it is not provided to the constructor, then L</openapi_uri> and
+L</openapi_schema> B<MUST> be provided, and L</evaluator> will also be used if provided.
 
-=head1 ACKNOWLEDGEMENTS
+=head2 openapi_uri
 
-...
+The URI that identifies the OpenAPI document.
+Ignored if L</openapi_document> is provided.
+
+=head2 openapi_schema
+
+The data structure describing the OpenAPI document. See spec link here TODO.
+Ignored if L</openapi_document> is provided.
+
+=head2 evaluator
+
+The L<JSON::Schema::Modern> object to use for all URI resolution and JSON Schema evaluation.
+Ignored if L</openapi_document> is provided.
+
+=head1 ACCESSORS
+
+=head2 openapi_document
+
+The L<JSON::Schema::Modern::Document::OpenAPI> document that holds the OpenAPI information to be
+used for validation.
+
+=head2 openapi_uri
+
+The URI that identifies the OpenAPI document.
+
+=head2 openapi_schema
+
+The data structure describing the OpenAPI document. See spec link here TODO.
+
+=head2 evaluator
+
+The L<JSON::Schema::Modern> object to use for all URI resolution and JSON Schema evaluation.
 
 =head1 SEE ALSO
 
 =for :list
-* L<foo>
+* L<foo> TODO!
 
 =cut
