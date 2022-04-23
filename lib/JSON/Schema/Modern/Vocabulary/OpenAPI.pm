81d96b41bad7dca0729a0e17785031bf2ae13650
Karen Etheridge <ether@cpan.org>
2022-04-23 09:49:13 -0700

    increment $VERSION after 0.026 release

--- lib/JSON/Schema/Modern/Vocabulary/OpenAPI.pm
+++ lib/JSON/Schema/Modern/Vocabulary/OpenAPI.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Modern::Vocabulary::OpenAPI;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Implementation of the JSON Schema OpenAPI vocabulary
 
-our $VERSION = '0.026';
+our $VERSION = '0.027';
 
 use 5.020;
 use Moo;
