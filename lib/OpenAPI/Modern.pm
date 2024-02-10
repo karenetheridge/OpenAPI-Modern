6fc90999a3c3dc753f3c99b578e78ea0fd68e188
Karen Etheridge <ether@cpan.org>
2024-02-09 18:30:25 -0800

    increment $VERSION after 0.059 release

--- lib/OpenAPI/Modern.pm
+++ lib/OpenAPI/Modern.pm
@@ -4,7 +4,7 @@ package OpenAPI::Modern;
 # ABSTRACT: Validate HTTP requests and responses against an OpenAPI v3.1 document
 # KEYWORDS: validation evaluation JSON Schema OpenAPI v3.1 Swagger HTTP request response
 
-our $VERSION = '0.059';
+our $VERSION = '0.060';
 
 use 5.020;
 use utf8;
