47181615d1a86910205cccb5db246156beff61c5
Karen Etheridge <ether@cpan.org>
2024-07-27 16:56:28 -0700

    increment $VERSION after 0.067 release

--- lib/JSON/Schema/Modern/Document/OpenAPI.pm
+++ lib/JSON/Schema/Modern/Document/OpenAPI.pm
@@ -4,7 +4,7 @@ package JSON::Schema::Modern::Document::OpenAPI;
 # ABSTRACT: One OpenAPI v3.1 document
 # KEYWORDS: JSON Schema data validation request response OpenAPI
 
-our $VERSION = '0.067';
+our $VERSION = '0.068';
 
 use 5.020;
 use Moo;
