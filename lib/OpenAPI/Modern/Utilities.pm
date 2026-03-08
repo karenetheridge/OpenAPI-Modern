3aea85821b56a8ad4ef6c294a506229318c47648
Karen Etheridge <ether@cpan.org>
2026-03-08 16:13:28 -0700

    increment $VERSION after 0.130 release

--- lib/OpenAPI/Modern/Utilities.pm
+++ lib/OpenAPI/Modern/Utilities.pm
@@ -3,7 +3,7 @@ package OpenAPI::Modern::Utilities;
 # vim: set ts=8 sts=2 sw=2 tw=100 et :
 # ABSTRACT: Internal utilities and common definitions for OpenAPI::Modern
 
-our $VERSION = '0.130';
+our $VERSION = '0.131';
 
 use 5.020;
 use strictures 2;
